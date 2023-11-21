b4ff16f320a608849505bc2a2af8406e1e33ab68
jasperwiese <jasper.wiese@protonmail.com>
2023-11-21 10:48:06 +0200

    Added support for systems variable selection for tomcat port.

--- src/main/java/dev/jasperwiese/ApplicationLauncher.java
+++ src/main/java/dev/jasperwiese/ApplicationLauncher.java
@@ -8,8 +8,13 @@ import org.apache.catalina.startup.Tomcat;
 
 public class ApplicationLauncher {
     public static void main(String[] args) throws LifecycleException {
+        int serverPort = 8080;
+        String portProperty = System.getProperty("server.port");
+        if(portProperty != null) {
+            serverPort = Integer.parseInt(portProperty);
+        }
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
+        tomcat.setPort(serverPort);
         tomcat.getConnector();
 
         Context ctx = tomcat.addContext("", null);
